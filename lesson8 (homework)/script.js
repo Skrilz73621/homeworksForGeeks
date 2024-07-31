4831cce9150802460eff2b4e2cc9713ce67c2dbc
Eugene Kiselev <jkiselev2411@gmail.com>
2024-07-31 10:58:19 +0600

    fix homework 8

--- lesson8 (homework)/script.js
+++ lesson8 (homework)/script.js
@@ -1,104 +1,45 @@
-const $months = document.querySelector('.months')
-const winterButton = document.querySelector('.winter')
-const springButton = document.querySelector('.spring')
-const summerButton = document.querySelector('.summer')
-const fallButton = document.querySelector('.fall')
+const $months = document.querySelector(".months");
+const $seasonButtons = document.querySelectorAll(".seasonButton");
 
-
-winterButton.addEventListener('click', renderWinterMonths)
-
-function renderWinterMonths(){
-    $months.innerHTML = ''
-    const december = winterMonths()
-    december.innerHTML = 'Декабрь'
-    const january = winterMonths()
-    january.innerHTML = 'Январь'
-    const february = winterMonths()
-    february.innerHTML = 'Февраль'
-    $months.append(december,january,february)
+for (const $seasonButton of $seasonButtons) {
+  $seasonButton.addEventListener("click", renderMonths);
 }
 
-function winterMonths(){
-    const winterMonth = document.createElement('button')
-    winterMonth.classList.add('seasonButton')
-    winterMonth.classList.add('winter')
-    
-    return winterMonth
+const seasons = {
+  Зима: {
+    monthes: ["Декабрь", "Январь", "Февраль"],
+    className: "winter",
+  },
+  Весна: {
+    monthes: ["Март", "Апрель", "Май"],
+    className: "spring",
+  },
+  Лето: {
+    monthes: ["Июнь", "Июль", "Август"],
+    className: "summer",
+  },
+  Осень: {
+    monthes: ["Сентябрь", "Октябрь", "Ноябрь"],
+    className: "autumn",
+  },
+};
+
+function renderMonths(event) {
+  const seasonName = event.target.innerText;
+  const season = seasons[seasonName];
+  $months.innerHTML = "";
+
+  for (const monthName of season.monthes) {
+    const button = MonthButton(monthName, season.className);
+    $months.append(button);
+  }
 }
 
+function MonthButton(text, className) {
+  const button = document.createElement("button");
+  button.classList.add("seasonButton");
+  button.classList.add(className);
+  button.innerText = text;
 
-
-
-
-
-
-springButton.addEventListener('click', renderSpringMonths)
-
-function renderSpringMonths(){
-    $months.innerHTML = ''
-    const march = springMonths()
-    march.innerHTML = 'Март'
-    const april = springMonths()
-    april.innerHTML = 'Апрель'
-    const may = springMonths()
-    may.innerHTML = 'Май'
-    $months.append(march,april,may)
+  return button;
 }
-
-function springMonths(){
-    const springMonth = document.createElement('button')
-    springMonth.classList.add('seasonButton')
-    springMonth.classList.add('spring')
-    
-    return springMonth
-}
-
-
-
-
-
-summerButton.addEventListener('click', renderSummerMonths)
-
-function renderSummerMonths(){
-    $months.innerHTML = ''
-    const june = summerMonths()
-    june.innerHTML = 'Июнь'
-    const july = summerMonths()
-    july.innerHTML = 'Июль'
-    const august = summerMonths()
-    august.innerHTML = 'Август'
-    $months.append(june,july,august)
-}
-
-function summerMonths(){
-    const summerMonths = document.createElement('button')
-    summerMonths.classList.add('seasonButton')
-    summerMonths.classList.add('summer')
-    
-    return summerMonths
-}
-
-
-
-
-fallButton.addEventListener('click', renderFallMonths)
-
-function renderFallMonths(){
-    $months.innerHTML = ''
-    const september = fallMonths()
-    september.innerHTML = 'Сентябрь'
-    const october = fallMonths()
-    october.innerHTML = 'Октябрь'
-    const november = fallMonths()
-    november.innerHTML = 'Ноябрь'
-    $months.append(september,october,november)
-}
-
-function fallMonths(){
-    const fallMonths = document.createElement('button')
-    fallMonths.classList.add('seasonButton')
-    fallMonths.classList.add('fall')
-    
-    return fallMonths
-}
-
